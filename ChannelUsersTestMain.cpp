1d9b90d226ad4f6d80ddad631af8f48b370a1ed4
iltafah <eliastafah@gmail.com>
2024-03-17 03:44:09 +0000

    delete some comments

--- ChannelUsersTestMain.cpp
+++ ChannelUsersTestMain.cpp
@@ -13,19 +13,16 @@ int main(int argc, char **argv) {
     int EliasUserId = 1337;
     User *user = db->addNewUser(EliasUserId, new User());
 
-
     //when `/join #uWu` command sent
     Channel* channel = db->getChannel("uWu");
     if (channel == NOT_FOUND) {
         std::cout << "channel not found, creating ...\n";
         channel = user->createChannel("uWu");
-        // db->addNewChannel("uWu", channel);
         if (db->getChannel("uWu") != NULL)
             std::cout << "Channel uWu has been created\n";
     }
     else {
         //ila kanet channel already kayna 4ay5ess user joini liha
-        // retrievedChannel->second->addUser(users[EliasUserId]);
         std::cout << "channel found, joining ...\n";
         channel->addUser(user);
     }
